f7419a40d6c75d906b37cd45c60142b05d244513
Omar <VenDev10@gmail.com>
2024-09-02 00:36:57 +0300

    Created Game

---
+++ TechHub 2024 Puzzle-Game Workshop/Assets/Tile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        print(other);
+        if(other.tag == "Player")
+            gameObject.SetActive(false);
+    }
+}
